5d0fce445f6d6c06cdb2ef07f2571843e39aa5df
Stênio do Carmo <126836443+steniodocarmo00@users.noreply.github.com>
2023-10-06 08:31:32 -0300

    comment UseEventUpdate.ts

--- src/hooks/UseEventUpdate.ts
+++ src/hooks/UseEventUpdate.ts
@@ -4,12 +4,12 @@ import { useMutation, useQueryClient } from "react-query";
 
 const API_URL = 'http://localhost:8080';
 
-const updateData = async ({ id, data }: { id: string; data: EventData }): AxiosPromise<any> => {
+const updateData = async ({ id, data }: { id: string; data: EventData }): AxiosPromise<any> => { //função assincrona que retorna requisição put
     const response = axios.put(`${API_URL}/event/${id}`, data);
     return response;
 }
 
-export function useEventUpdate() {
+export function useEventUpdate() { //cria uma mutação dos dados definindo a função, key e tentativa
     const queryClient = useQueryClient();
     const mutate = useMutation(updateData, {
         retry: 2,
@@ -18,5 +18,5 @@ export function useEventUpdate() {
         }
     });
 
-    return mutate;
+    return mutate; //retorna a mutação
 }
